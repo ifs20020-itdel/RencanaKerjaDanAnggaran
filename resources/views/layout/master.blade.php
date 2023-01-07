5918933bc2547fbb07d7a35ba80aabf5847b501a
ifs20020-itdel <ifs20020@students.del.ac.id>
2023-01-07 23:28:18 +0700

    Update Nav and Master blade

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -1,106 +1,103 @@
-<!--
-    NIM  : 11S20020
-    Nama : Roosen Gabriel Manurung 
--->
-
 <!DOCTYPE html>
 <html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Fixed Navbar Layout</title>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>11S20020 - Roosen Gabriel Manurung</title>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="{{asset('layout/plugins/fontawesome-free/css/all.min.css')}}">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{asset('layout/dist/css/adminlte.min.css')}}">
+</head>
+<body class="hold-transition sidebar-mini layout-navbar-fixed">
+<!-- Site wrapper -->
+<div class="wrapper">
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{asset("layout/plugins/fontawesome-free/css/all.min.css")}}">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{asset("layout/dist/css/adminlte.min.css")}}">
-    <link rel="icon" href="{{asset("layout/dist/img/del.ico")}}" type="image/x-icon">
-    @stack('style')
 
-    
-  </head>
+  <!-- Navbar -->
+  @include('partial.nav')
+  <!-- /.navbar -->
 
-  <body class="hold-transition sidebar-mini">
-    <!-- Site wrapper -->
-    <div class="wrapper">
+  <!-- Main Sidebar Container -->
+  @include('partial.sidebar')
 
-    <!-- Navbar -->
-    @include('partial.nav')
-    <!-- /.navbar -->
-
-    <!-- Main Sidebar Container -->
-    @include('partial.sidebar')
-
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <div class="container-fluid">
-            <div class="row mb-2">
-              <div class="col-sm-6">
-                <h1>@yield('judul')</h1>
-              </div>
-            </div>
-          </div><!-- /.container-fluid -->
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-
-          <!-- Default box -->
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">@yield('title')</h3>
-
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
-            <div class="card-body">
-              @yield('content')
-            </div>
-            <!-- /.card-body -->
-            <!-- /.card-footer-->
-          </div>
-          <!-- /.card -->
-
-        </section>
-        <!-- /.content -->
-      </div>
-      <!-- /.content-wrapper -->
-      <br>
-      <footer class="main-footer">
-        <div class="float-right d-none d-sm-block">
-          <b>11S20020</b>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      
+      <div class="container-fluid">
+        <div class="row ml-1 mb-2">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            @yield('breadcrumb1')
+            @yield('breadcrumb2')
+          </ol>
         </div>
-        <strong>Copyright &copy; 2022 Roosen G.M.</a></strong>
-      </footer>
+        <hr>
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>@yield('judul')</h1>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
 
-      <!-- Control Sidebar -->
-      <aside class="control-sidebar control-sidebar-dark">
-        <!-- Control sidebar content goes here -->
-      </aside>
-      <!-- /.control-sidebar -->
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12">
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">@yield('title')</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+                @yield('content')
+              </div>
+              <!-- /.card-body -->
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Rencana Kerja Dan Anggaran</b>
     </div>
-    <!-- ./wrapper -->
+    <strong>Copyright &copy; 2023 -</strong> <a href="https://coda.io/d/RKA_dGvkMHI5SK0/Rencana-Kerja-dan-Anggaran-Institut-Teknologi-Del-T-A-2022-2023_su8Ub#_luXXM" target="_blank"> Kelompok 1</a> 
+  </footer>
 
-    <!-- jQuery -->
-    <script src="{{asset("layout/plugins/jquery/jquery.min.js")}}"></script>
-    <!-- Bootstrap 4 -->
-    <script src="{{asset("layout/plugins/bootstrap/js/bootstrap.bundle.min.js")}}"></script>
-    <!-- AdminLTE App -->
-    <script src="{{asset("layout/dist/js/adminlte.min.js")}}"></script>
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
 
-    @stack('scripts')
+<!-- jQuery -->
+<script src="{{asset('layout/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('layout/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('layout/dist/js/adminlte.min.js')}}"></script>
 
-  </body>
-  
+</body>
 </html>
