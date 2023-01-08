9cbcfafe257539fb5dbf72a028d4c7e30736d9b7
ifs20020-itdel <ifs20020@students.del.ac.id>
2023-01-08 21:01:08 +0700

    Template RKA

--- resources/views/pendidikan/a/create.blade.php
+++ resources/views/pendidikan/a/create.blade.php
@@ -10,7 +10,8 @@
 @section('judul', 'A. Biaya Dosen (Gaji Dan Honor)')
 
 @section('content')
-  
+<h6>Berikut Panduan Template RKA  <a href="https://docs.google.com/spreadsheets/d/140zs3W8NE7GwuaQlNXegL6atDtKjO4y7/edit#gid=712992635" target="_blank"><span class="badge badge-success ml-1">Template RKA</span></a></h6>
+<br>
 <div class="col-lg-7 col-6">
     <div class="card card-dark">
         <div class="card-header">
