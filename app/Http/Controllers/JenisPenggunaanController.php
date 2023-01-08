13fe0eb80a75070e6136d58e3c6b9ff59c7fd05b
ifs20020-itdel <ifs20020@students.del.ac.id>
2023-01-09 00:38:05 +0700

    Controller

--- app/Http/Controllers/JenisPenggunaanController.php
+++ app/Http/Controllers/JenisPenggunaanController.php
@@ -10,7 +10,7 @@ class JenisPenggunaanController extends Controller
     public function operasionalPendidikan(){
         return view('pendidikan.operasionalPendidikan');
     }
-
+//===================================================================================================================================
     //Operasional Pendidikan
         //A.Dosen
         public function biayaDosenGenreCreate(){
@@ -77,6 +77,70 @@ class JenisPenggunaanController extends Controller
             DB::table('biaya_dosen_genre')->where('id', '=', $id)->delete();
             return redirect('/biayaOperasionalPendidikan');
         }
+
+//===================================================================================================================================
+    //Operasional Pendidikan
+        //B. Tenaga Kependidikan
+    public function biayaDosenGenreStore(Request $request){
+            $request->validate([
+                'namaAnggaran' => 'required',
+                'mataAnggaran' => 'required',
+            ],
+            [
+                'namaAnggaran.required' => 'Nama Anggaran Harus Di Isi',
+                'mataAnggaran.required' => 'Mata Anggaran Harus Di Isi',
+            ]);
+            DB::table('biaya_dosen_genre')->insert(
+                [
+                    'namaAnggaran' => $request['namaAnggaran'],
+                    'mataAnggaran' => $request['mataAnggaran']
+                ]
+            );
+            
+            return redirect('/biayaOperasionalPendidikan');
+        }
+
+        public function biayaDosenGenreIndex(){
+            $biayaDosenGenre = DB::table('biaya_dosen_genre')->get();
+
+            return view('pendidikan.operasionalPendidikan', compact('biayaDosenGenre')); //namaVariabel
+        }
+
+        public function biayaDosenGenreShow($id){
+            $biayaDosenGenre = DB::table('biaya_dosen_genre')->where('id', $id)->first();
+
+            return view('pendidikan.a.show', compact('biayaDosenGenre')); //namaVariabel
+        }
+
+        public function biayaDosenGenreEdit($id){
+            $biayaDosenGenre = DB::table('biaya_dosen_genre')->where('id', $id)->first();
+
+            return view('pendidikan.a.edit', compact('biayaDosenGenre')); //namaVariabel
+        }
+
+        public function biayaDosenGenreUpdate($id, Request $request){
+            $request->validate([
+                'namaAnggaran' => 'required',
+                'mataAnggaran' => 'required',
+            ],
+            [
+                'namaAnggaran.required' => 'Nama Anggaran Harus Di Isi',
+                'mataAnggaran.required' => 'Mata Anggaran Harus Di Isi',
+            ]);
+            DB::table('biaya_dosen_genre')->where('id', $id)
+                ->update(
+                    [
+                        'namaAnggaran' => $request['namaAnggaran'],
+                        'mataAnggaran' => $request['mataAnggaran']
+                    ]
+                );
+            
+            return redirect('/biayaOperasionalPendidikan');
+        }
         
+        public function biayaDosenGenreDestroy($id){
+            DB::table('biaya_dosen_genre')->where('id', '=', $id)->delete();
+            return redirect('/biayaOperasionalPendidikan');
+        }    
 
 }
